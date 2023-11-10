31b54494c5994de5b60c5a6672061f4961785e16
CAMARA04 <loic.camara@gmail.com>
2023-11-10 19:12:00 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ipsa fuga
+        quidem ut veritatis enim labore quia facere, impedit doloribus corrupti
+        blanditiis velit in delectus. Molestiae velit perspiciatis dolore!
+        Nostrum eveniet eos earum facere dolores libero asperiores explicabo
+        quam, et facilis suscipit quisquam hic esse fuga molestiae animi
+        inventore necessitatibus quae odit delectus nesciunt doloremque
+        praesentium quo possimus! Iure ullam, molestiae facere velit at alias
+        amet! Tempore vel iste harum quia repudiandae sequi repellat quos. Quod
+        natus quos quas ipsam ad repudiandae beatae blanditiis minus non
+        perferendis, adipisci aut molestias quam nobis id assumenda cupiditate
+        nulla illum eveniet nesciunt est!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore hic
+        maiores sunt, dignissimos quibusdam vero voluptate facilis dicta. Quis
+        in reiciendis facilis nisi explicabo cupiditate, id nulla enim
+        exercitationem, doloribus quam eaque inventore vero. Quibusdam eius quam
+        iste eligendi reprehenderit ea porro corrupti quae eaque incidunt!
+        Praesentium placeat maxime libero doloremque eum minus. Nobis reiciendis
+        eaque modi repudiandae neque, officia sapiente tempora veniam. Magni
+        quos aliquam totam excepturi dolores fugiat qui quo, blanditiis ipsum
+        debitis aliquid cupiditate tempore provident rem accusamus hic
+        aspernatur iure dolorem nulla distinctio saepe natus mollitia facere
+        nemo? Perferendis modi unde rerum obcaecati architecto, quae quidem.
+      </p>
+    </div>
+  );
+};
+
+export default About;
